8522f3c470618e14f9043fe470f6b33999f37486
Brittany <sincerelybr@gmail.com>
2020-11-03 13:54:16 -0500

    final updates

--- src/App.js
+++ src/App.js
@@ -1,42 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { Title } from "./Styles";
+import List from "./List";
+import Form from "./Form";
 import "./styles.css";
-import List from './List';
-import Form from './Form'
 
 export default function App() {
-  // const items = ["wake up", "code"]
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      item: "wakeUP",
-      priority: "high"
-    } 
-    // {
-    //   id: 2,
-    //   item: "wakeUP",
-    //   priority: "high"
-    // }
-  ])
+  const [listItems, setListItems] = useState([]);
 
-  const handleDelete = (id) => {
-    const newItems = items.filter(i => i.id !== id)
-    setItems(newItems)
-  }
+  const handleItemDelete = id => {
+    const newListItems = listItems.filter(li => li.id !== id);
+    setListItems(newListItems);
+  };
 
-  const handleFormSubmit = (itemInfoArr) => {
-    const newItems = [...items, {
-      id: `${itemInfoArr[0]}${itemInfoArr[1]}`,
-      item: itemInfoArr[0],
-      priority: itemInfoArr[1]
-    }]
-    setItems(newItems)
-  }
-
+  const handleFormSubmit = formData => {
+    setListItems([
+      ...listItems,
+      {
+        item: formData[0],
+        priority: formData[1],
+        id: `${formData[0]}${formData[1]}`
+      }
+    ]);
+  };
   return (
     <div className="App">
-      <h1>Todos</h1>
-      <Form submitCallback={handleFormSubmit}/>
-      <List items={items} handleDelete={handleDelete}/>
+      {/* <Title>Todos:</Title> */}
+      <Form
+        submitCallback={handleFormSubmit}
+        inputs={["Item", "Priority"]}
+        submitValue={"New Todo"}
+      />
+      <List items={listItems} handleDelete={handleItemDelete} />
     </div>
   );
 }
+
+// import React, { useState, useEffect } from "react";
+// import "./styles.css";
+// import List from './List';
+// import Form from './Form'
+
+// export default function App() {
+//   // const items = ["wake up", "code"]
+//   const [items, setItems] = useState([
+//     {
+//       id: 1,
+//       item: "wakeUP",
+//       priority: "high"
+//     } 
+//     // {
+//     //   id: 2,
+//     //   item: "wakeUP",
+//     //   priority: "high"
+//     // }
+//   ])
+
+//   const handleDelete = (id) => {
+//     const newItems = items.filter(i => i.id !== id)
+//     setItems(newItems)
+//   }
+
+//   const handleFormSubmit = (itemInfoArr) => {
+//     const newItems = [...items, {
+//       id: `${itemInfoArr[0]}${itemInfoArr[1]}`,
+//       item: itemInfoArr[0],
+//       priority: itemInfoArr[1]
+//     }]
+//     setItems(newItems)
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>Todos</h1>
+//       <Form submitCallback={handleFormSubmit}/>
+//       <List items={items} handleDelete={handleDelete}/>
+//     </div>
+//   );
+// }
--- src/Form.js
+++ src/Form.js
@@ -1,37 +1,79 @@
-import React, { useState } from 'react'
 
-export default function Form(props){
+import React, { useState } from "react";
 
-  const [itemText, setItemText] = useState('')
-  const [priorityText, setPriorityText] = useState('')
+export default function Form(props) {
+  const [inputs, setInputs] = useState(props.inputs.map(input => ""));
 
-  const handleItemInputChange = (e) =>{
-    //you have control over what someone puts into a form here
-    setItemText(e.target.value)
-  }
+  const handleTextChange = (e, i) => {
+    const nextInputs = [...inputs];
+    nextInputs[i] = e.target.value;
+    setInputs(nextInputs);
+  };
 
-  const handlePriorityInputChange = (e) =>{
-    setPriorityText(e.target.value)
-  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    setInputs(props.inputs.map(i => ""));
+    props.submitCallback(inputs);
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setPriorityText("")
-    setItemText("")
-    props.submitCallback(itemText, priorityText)
-  }
+  const renderInputs = () => {
+    return props.inputs.map((inputName, index) => (
+      <input
+        type="text"
+        onChange={e => handleTextChange(e, index)}
+        value={inputs[index]}
+        placeholder={inputName}
+        key={inputName}
+      />
+    ));
+  };
 
-return(
-      <form> 
-        <input type="text" 
-        placeholder="item" 
-        onChange={handleItemInputChange} 
-        value={itemText}/>
-        <input type="text" 
-        placeholder="priority" 
-        onChange={handlePriorityInputChange} 
-        value={priorityText}/>
-        <button onClick={handleSubmit}> New Todo </button> 
-      </form>
-    )
-  }
+  return (
+    <form>
+      {renderInputs()}
+      <button type="submit" onClick={handleSubmit}>
+        {props.submitValue}
+      </button>
+    </form>
+  );
+}
+
+
+// import React, { useState } from 'react'
+
+// export default function Form(props){
+
+//   const [itemText, setItemText] = useState('')
+//   const [priorityText, setPriorityText] = useState('')
+
+//   const handleItemInputChange = (e) =>{
+//     //you have control over what someone puts into a form here
+//     console.log(e.target.value)
+//     setItemText(e.target.value)
+//   }
+
+//   const handlePriorityInputChange = (e) =>{
+//     setPriorityText(e.target.value)
+//   }
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+//     setPriorityText("")
+//     setItemText("")
+//     props.submitCallback(itemText, priorityText)
+//   }
+
+// return(
+//       <form> 
+//         <input type="text" 
+//         placeholder="item" 
+//         onChange={handleItemInputChange} 
+//         value={itemText}/>
+//         <input type="text" 
+//         placeholder="priority" 
+//         onChange={handlePriorityInputChange} 
+//         value={priorityText}/>
+//         <button onClick={handleSubmit}> New Todo </button> 
+//       </form>
+//     )
+//   }
