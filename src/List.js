06dfb95c738157b8e1132e428f502ceea916f534
Brittany <sincerelybr@gmail.com>
2020-11-02 22:40:52 -0500

    Setups up the list document Sets up list.js

---
+++ src/List.js
@@ -0,0 +1,36 @@
+// import React, {Component} from 'react'
+
+// export default class List extends Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {
+
+//     }
+//   }
+
+// someMethod(){
+//   const newItem = "wakeUp"
+//   this.setState({items:[newItem]})
+
+//   this.setState(prevState => ({
+//     items: [...prevState.items, newItem]
+//   }), () => {
+//     //then do this
+//   })
+// }
+// }
+
+import React, { useState, useEffect } from "react";
+
+export default function List() {
+  const [items, setItems] = useState([]);
+
+  //items = what my state is/ what I want the initial value of my state to be
+  //serItems = a function to set my state
+
+  const myMethod = () => {
+    setItems([...items, "wake up"]);
+  };
+
+  useEffect(() => {});
+}
