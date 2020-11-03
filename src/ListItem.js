42748ea6685d6771fa9ce09f8748eb37cfaa1a9d
Brittany <sincerelybr@gmail.com>
2020-11-03 12:15:50 -0500

    creates delete button and onlick for the deletebutton

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import {Card} from './Styles'
+import {Card, RedButton} from './Styles'
 
 export default function ListItem(props){
+
+  const handleDelete = (e) => {
+      e.preventDefault()
+      return props.deleteCallback(props.id)
+  }
+
   return(
     <Card>
       <h4> Item: {props.item} </h4> 
       <p> Priority: {props.priority} </p>
+      <RedButton onClick={handleDelete}> Delete </RedButton>
     </Card>
   )
 }
