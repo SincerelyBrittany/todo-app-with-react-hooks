9354772221432250593bc9fc5798f46e6f60cc8e
Brittany <sincerelybr@gmail.com>
2020-11-03 11:06:29 -0500

    Adds card styles

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Card} from './Styles'
 
 export default function ListItem(props){
   return(
-    <div>
+    <Card>
       <h4> Item: {props.item} </h4> 
       <p> Priority: {props.priority} </p>
-    </div>
+    </Card>
   )
 }
---
+++ src/Styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+const Card = styled.div`
+border: 1px solid black;
+box-shadow: 5px 10px black;
+padding:5px;
+margin: 20px;
+`
+
+export {Card}
